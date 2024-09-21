87af5a75e391b9d527cdd4e468e68fd0dcd4114c
Ingenious5V <j.luhvsims@gmail.com>
2024-09-21 15:54:48 -0400

    config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -1,28 +1,38 @@
 Config = {}
 
 Config.Apartments = {
-    {name = "Alta Street", price = 500},
-    {name = "Integrity Way", price = 750},
-    {name = "Tinsel Towers", price = 1000}
+    {
+        name = "Downtown Apartment",
+        price = 1200,
+        interiors = {
+            {shell = "shell_v16_low", price = 500},
+            {shell = "shell_v16_mid", price = 750},
+            {shell = "shell_v16_high", price = 1000}
+        }
+    },
+    {
+        name = "Beachside Apartment",
+        price = 1800,
+        interiors = {
+            {shell = "shell_v16_low", price = 500},
+            {shell = "shell_v16_mid", price = 750},
+            {shell = "shell_v16_high", price = 1000}
+        }
+    },
+    {
+        name = "Luxury Penthouse",
+        price = 2500,
+        interiors = {
+            {shell = "shell_v16_low", price = 500},
+            {shell = "shell_v16_mid", price = 750},
+            {shell = "shell_v16_high", price = 1000}
+        }
+    }
+    -- Add more apartments as needed
 }
 
-Config.Interiors = {
-    {name = "Modern", id = 1},
-    {name = "Classic", id = 2},
-    {name = "Vintage", id = 3},
-    {name = "K4MB1 Shell 1", id = 4},
-    {name = "K4MB1 Shell 2", id = 5},
-    {name = "K4MB1 Shell 3", id = 6}
+Config.PedMarkerLocation = {
+    x = -267.0, y = -960.0, z = 31.0  -- Example coordinates for the ped marker
+    -- Update with the actual coordinates for your ped marker
 }
 
--- Function to add more shells
-function Config.AddShell(name, id)
-    table.insert(Config.Interiors, {name = name, id = id})
-end
-
-Config.PedLocation = {
-    x = -268.0,
-    y = -957.0,
-    z = 31.0,
-    heading = 200.0
-}
